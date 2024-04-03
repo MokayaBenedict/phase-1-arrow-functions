ed5e55fbfb76bcc66f8d91bbfff931bb764bf3bb
Benedict <mokayaakivatsi@gmail.com>
2024-04-04 00:21:38 +0300

    done arrow fuctions

---
+++ index.js
@@ -0,0 +1,16 @@
+// function divide (){
+// let a =2000;
+// let b=100;
+// return (a/b);
+
+// }
+//
+
+ let a = 2000;
+ let b = 100;
+ divide=(a, b) => a / b;
+ let x = 4;
+ square = (x) => x * x;
+ let m = 3;
+ let y = 6;
+ add =(m ,y )=> m + y;
